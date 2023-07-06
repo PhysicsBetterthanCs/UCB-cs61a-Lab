3ce332c75037bb6883614dd71c4affb050566ab6
PhysicsBetterthanCs <132644417+PhysicsBetterthanCs@users.noreply.github.com>
2023-07-06 13:34:15 +0800

    Add files via upload

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,9 @@
+def twenty_twenty_three():
+    """Come up with the most creative expression that evaluates to 2023,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_three()
+    2023
+    """
+    x = 2 * 1000 + 2 * 10 + 3
+    return x
